fe3da6614a631b99c7df9ac86162984cd8046a02
Kasahara1341 <kasahara-tsuyoshi@ed.tmu.ac.jp>
2025-09-08 13:12:40 +0900

    09/08自宅⇒研究室 円柱周り流れの解析において，流体静止，円柱強制移動の解析フォルダを作成(Karman_moving)

---
+++ Karman_vortex_movingIB/C_D.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+csv_file = 'C_D.csv'
+f = csv.reader(open(csv_file, 'r'), delimiter=',', doublequote=True, lineterminator='\r\n',
+               quotechar='"', skipinitialspace=True)
+C_D = []
+C_time = []
+for row in f:
+    if row[0] != 'C_D':
+        C_D.append(float(row[0]))
+        C_time.append(float(row[1]))
+plt.plot(C_time, C_D, marker='o', linestyle='-', color='b')
+plt.xlabel('Time (s)')
+plt.ylabel('C_D')
+plt.title('Drag Coefficient C_D over Time')
+plt.grid()
+# plt.xlim(300,350)
+# plt.ylim(1.5,1.55)
+# plt.xlim(150,400)
+# plt.ylim(1.2,1.6)
+plt.savefig('C_D_plot.png')
+plt.show()
